fef828df01964d030b76306339cea7d8bf895be3
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-23 17:16:25 +0530

    chore: next previous and today button changes
    1. refactor to use include calendar

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const path = require('path');
+const pug = require('pug');
 
 const PORT = process.env.PORT || 3000;
 
-  const dayNames = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday','Friday','Saturday'];
+let currentMonth;
+let currentYear;
+const dayNames = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday','Friday','Saturday'];
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('assets'));
 
 function getCalendarRows(month, year) {
   const daysInMonth = 32 - new Date(year, month, 32).getDate();
-  console.log(daysInMonth);
+  //console.log('days in month: ' + daysInMonth);
   const firstDay = new Date(year, month, 1).getDay();
-  console.log(dayNames[firstDay]);
+  //console.log('first day of month: ' + dayNames[firstDay]);
   const firstWeekDays = 7 - firstDay;
+  //console.log('first week days: ' + firstWeekDays);
   const remainingDays = daysInMonth - firstWeekDays;
-  const rowsNeeded = remainingDays / 7 ;
+  //console.log('remaining days: ' + remainingDays);
+  const rowsNeeded = Math.ceil(remainingDays / 7) ;
+  //console.log('rows needed: ' + rowsNeeded);
 
   const emptyCols = ['','','','','','',''];
   const rows = [];
@@ -25,16 +46,22 @@ function getCalendarRows(month, year) {
 
   // populate 1st week
   let cols = emptyCols;
+  let x = 1;
   for(let j = firstDay; j< 7;j++) {
-    cols[j] = j;
+    cols[j] = x++;
   }
   rows[0] = cols;
 
   // populate the remaining rows
-  for(let i = 1; i< 5;i++) {
+  for(let i = 1; i <= rowsNeeded;i++) {
     const temp = [];
     for(let j = 0;j < 7; j++) {
-      temp[j] = (i* firstWeekDays) + (j );
+      const dt = firstWeekDays + ((i -1) * 7) + (j + 1) ;
+      if(dt <= daysInMonth) {
+        temp[j] =   dt;  
+      } else {
+        temp[j] = '';
+      }
     }
     rows[i] = temp;
   }
@@ -42,45 +69,52 @@ function getCalendarRows(month, year) {
   return rows;
 }
 
+function getMarkup(currentMonth, currentYear) {
+  const rows = getCalendarRows(currentMonth, currentYear)
+  const template = pug.compileFile('views/calendar.pug');
+  const monthYear = `${monthNames[currentMonth]} - ${currentYear}`;
+  const markup = template({ dayNames, rows, currentMonth, monthYear, currentYear });
+  return markup;
+}
+
 app.get('/', (req, res) => {
   const today = new Date();
-  const currentMonth = today.getMonth();
-  const currentYear = today.getFullYear();
-  /*
-  const daysInMonth = 32 - new Date(currentYear, currentMonth, 32).getDate();
-  console.log(daysInMonth);
-  const firstDay = new Date(currentYear, currentMonth, 1).getDay();
-  console.log(dayNames[firstDay]);
-  const firstWeekDays = 7 - firstDay;
-  const remainingDays = daysInMonth - firstWeekDays;
-  const rowsNeeded = remainingDays / 7 ;
-
-  const emptyCols = ['','','','','','',''];
-  const rows = [];
-  for(let i =0;i<5;i++) {
-    rows[i] = emptyCols;
-  }
-
-  // populate 1st week
-  let cols = emptyCols;
-  for(let j = firstDay; j< 7;j++) {
-    cols[j] = j;
-  }
-  rows[0] = cols;
-
-  // populate the remaining rows
-  for(let i = 1; i<= 4;i++) {
-    const temp = [];
-    for(let j = 0;j < 7; j++) {
-      temp[j] = (i* firstWeekDays) + (j + 1);
-    }
-    rows[i] = temp;
-  }
-  */
+  currentMonth = today.getMonth();
+  currentYear = today.getFullYear();
   const rows = getCalendarRows(currentMonth, currentYear)
-
-  res.render('index', { dayNames, rows });
+  const monthYear = `${monthNames[currentMonth]} - ${currentYear}`;
+  res.render('index', { dayNames, rows, currentMonth, monthYear, currentYear });
 });
+
+app.get('/next', (req,res) => {
+  currentMonth += 1;
+  if(currentMonth > 11 ) {
+    currentMonth = 0;
+    currentYear += 1;
+  }
+  const markup = getMarkup(currentMonth, currentYear);
+  res.send(markup);
+});
+
+app.get('/previous', (req,res) => {
+  currentMonth -= 1;
+  if(currentMonth < 0 ) {
+    currentMonth = 11;
+    currentYear -= 1;
+  }
+  const markup = getMarkup(currentMonth, currentYear);
+  res.send(markup);
+});
+
+
+app.get('/today', (req,res) => {
+  const today = new Date();
+  currentMonth = today.getMonth();
+  currentYear = today.getFullYear();
+  const markup = getMarkup(currentMonth, currentYear);
+  res.send(markup);
+});
+
 
 app.listen(PORT);
 console.log('Listening on port: ' + PORT);
