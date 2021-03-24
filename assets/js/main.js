cfcf20d70c332b0c3dc9a125753976e6917ccdc6
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-24 13:08:52 +0530

    chore: add event delete functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,7 @@ function closeModal() {
   }, 200)
 }
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
